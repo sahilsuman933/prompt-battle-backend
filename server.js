1c75133fee3851248045ba9342ddcb6788fffdb4
Sahil Suman <sahilsuman933@gmail.com>
2022-12-03 22:26:41 +0530

    added cors
    Signed-off-by: Sahil Suman <sahilsuman933@gmail.com>

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import router from "./routes";
+import cors from "cors";
 
 const app = express();
 
 const port = 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
