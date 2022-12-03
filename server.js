d1be6dc43eb31080ab4d2bf3311ad8025cebfc9f
Sahil Suman <sahilsuman933@gmail.com>
2022-12-04 00:03:03 +0530

    Access Log File
    Signed-off-by: Sahil Suman <sahilsuman933@gmail.com>

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 import express from "express";
 import router from "./routes";
 import cors from "cors";
+import path from "path";
+import fs from "fs";
+import bodyParser from "body-parser";
+import morganBody from "morgan-body";
 
 const app = express();
+var log = fs.createWriteStream(path.join(__dirname, "access.log"), {
+  flags: "a",
+});
 
 const port = 8080;
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
+morganBody(app, {
+  noColors: true,
+  prettify: true,
+  logRequestBody: true,
+  stream: log,
+});
+
 app.use("/api", router);
 
 app.listen(port, () => {
